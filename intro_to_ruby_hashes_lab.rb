1c58907ba13d5ef3f15492fb3bd36b3694e77fb6
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-06-24 14:58:30 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,29 +1,98 @@
 def new_hash
-  # return an empty hash
+
+ 	test_hash = Hash.new
 end
 
-def my_hash
-  # return a valid hash with any key/value pair of your choice
+
+def actor
+
+ 	actor = {name: "Dwayne The Rock Johnson"}
 end
 
-def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+
+ def monopoly
+   monopoly = {
+		:railroads=>{}
+	}
+
+
+ end
+
+
+ def monopoly_with_second_tier
+   monopoly = {
+
+ 		:railroads=>
+		{
+		:pieces=>4,
+		:names=>{},
+		:rent_in_dollars=>{}
+		}
+	}
+
+ end
+
+def monopoly_with_third_tier
+	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!		monopoly = {
+
+ 		:railroads=>
+end			{
+		:pieces=>4,
+		:names=>
+		{
+			:reading_railroad=>{},
+			:pennsylvania_railroad=>{},
+			:b_and_o_railroad=>{},
+			:shortline=>{}
+		},
+		:rent_in_dollars=>
+		{
+			:one_piece_owned=>25,
+			:two_pieces_owned=>50,
+			:three_pieces_owned=>100,
+			:four_pieces_owned=>200
+		}
+		}
+	}
+
 end
 
-def id_generator
-  # return a hash with a key :id assigned to positive integer
-end
 
-def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
-end
+ def monopoly_with_fourth_tier
+	monopoly = {
+		:railroads=>
+		{
+		:pieces=>4,
+		:names=>
+		{
+			:reading_railroad=>
+			{
+				"mortgage_value"=>"$100"
+			},
+			:pennsylvania_railroad=>
+			{
+				"mortgage_value"=>"$200"
+			},
+			:b_and_o_railroad=>
+			{
+				"mortgage_value"=>"$400"
+			},
+			:shortline=>
+			{
+				"mortgage_value"=>"$800"
+			}
+		},
+		:rent_in_dollars=>
+		{
+			:one_piece_owned=>25,
+			:two_pieces_owned=>50,
+			:three_pieces_owned=>100,
+			:four_pieces_owned=>200
+		}
+		}
+	}
 
-def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
-end
 
-def update_counting_hash(hash, key)
-  # given a hash an a key as parameters, return an updated hash
-  # if the provided key is not present in the hash, add it and assign it to the value of 1
-  # if the provided key is present, increment its value by 1
-end
+
+ end
+
